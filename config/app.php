f681985a54a00bb4ee86b65992b2a43883d8ef60
Parveen <parveen.kngr@gmail.com>
2016-04-26 23:25:40 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -226,8 +226,8 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'root',
-            'password' => '',
+            'username' => 'sportspaluser',
+            'password' => 'v4SVBQrV2E56f82onENnUa8mt',
             'database' => 'sportspal',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
